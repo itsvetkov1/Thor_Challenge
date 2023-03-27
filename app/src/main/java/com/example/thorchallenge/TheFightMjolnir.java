0c25c7929d4a901fd51d663c4ef5e12089d5930b
itsvetkov1 <i.b.cvetkov@gmail.com>
2023-03-27 09:44:22 +0300

    in_progress

--- app/src/main/java/com/example/thorchallenge/TheFightMjolnir.java
+++ app/src/main/java/com/example/thorchallenge/TheFightMjolnir.java
@@ -30,7 +30,7 @@ public class TheFightMjolnir extends AppCompatActivity {
         buttonday1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                redirectTo("jump_jacks.html");
+                redirectTo("jump_jacks_1_d1.html");
             }
         });
 
